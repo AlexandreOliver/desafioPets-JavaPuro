a01f71bdebc9fa51e5679f281ad6a38e736b660e
AlexandreOliver <alexandreferreiradeoliveira40@gmail.com>
2026-09-11 21:00:01 -0300

    feat: Passo 1 - Leitura de Arquivo base
    - Criada a pasta resources/ para futuros arquivos

--- src/CadastroApplication.java
+++ src/CadastroApplication.java
@@ -1,9 +1,22 @@
-
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class CadastroApplication {
+
+  private final static File formularioSource = new File("src/resources/formulario.txt");
+
   public static void main(String[] args) {
 
+    try (Scanner scanner = new Scanner(formularioSource)) {
 
+      while (scanner.hasNext()) {
+        System.out.println(scanner.nextLine());
+      }
+
+    } catch (FileNotFoundException fileError) {
+      System.out.println("Arquivo formulario.txt não encontrado");
+    }
 
   }
 }
